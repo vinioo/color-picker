95ce3a52d50e1baaf65c7ef14b031171fc083abb
Vinioo <vi.ni.cius4@hotmail.com>
2019-06-18 13:44:05 -0300

    Start project

---
+++ src/Palette.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+import './Palette.css';
+
+export default class Pallete extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { level: 500 };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+    changeLevel(level) {
+        this.setState({
+            level
+        });
+    }
+    render() {
+        const { colors } = this.props.palette;
+        const { level } = this.state;
+        const colorBoxes = colors[level].map(color => (
+            <ColorBox background={color.hex} name={color.name} />
+        ));
+        return (
+            <div className="Palette">
+                <Navbar level={level} changeLevel={this.changeLevel} />
+                <div className="Palette-colors">{colorBoxes}</div>
+                {/* Footer */}
+            </div>
+        );
+    }
+}
