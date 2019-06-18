fbcfb9d349c960ce95913a5c4c47d9498e42fdc0
Vinioo <vi.ni.cius4@hotmail.com>
2019-06-18 14:03:29 -0300

    Add select and materialUi

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -20,7 +20,7 @@ function generatePalette(starterPalette) {
                 id: color.name.toLowerCase().replace(/ /g, '-'),
                 hex: scale[i],
                 rgb: chroma(scale[i]).css(),
-                rgba: chroma(scale[i]).css().replace("rgba","rgba").replace(")", "1.0")
+                rgba: chroma(scale[i]).css().replace("rgb","rgba").replace(")", "1.0")
             });
         }
     }
