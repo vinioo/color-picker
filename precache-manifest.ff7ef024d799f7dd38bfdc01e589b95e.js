e074ae1d29489f0cdf9af645150730aa5a1f3ae3
Vinicius <vi.ni.cius4@hotmail.com>
2019-07-15 15:06:46 -0300

    Updates

---
+++ precache-manifest.ff7ef024d799f7dd38bfdc01e589b95e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbed208c8a39c4d0013d1c25e934527",
+    "url": "/colorpicker/index.html"
+  },
+  {
+    "revision": "3afee0190f9eb8307dad",
+    "url": "/colorpicker/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "25f5857044c8e6d04c5c",
+    "url": "/colorpicker/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3afee0190f9eb8307dad",
+    "url": "/colorpicker/static/js/2.805fd14e.chunk.js"
+  },
+  {
+    "revision": "25f5857044c8e6d04c5c",
+    "url": "/colorpicker/static/js/main.5214bd67.chunk.js"
+  },
+  {
+    "revision": "adc9ca72dd11a667a1a6",
+    "url": "/colorpicker/static/js/runtime~main.c09bf667.js"
+  }
+]);
